690cb063ac7be37c4eb320508e80affd29fb9850
wighs33 <genji@kakao.com>
2023-01-21 11:09:27 +0900

    C_Hero 클래스 최적화하기

--- HeroChess/C_Hero.cpp
+++ HeroChess/C_Hero.cpp
@@ -1,30 +1,7 @@
 #include "stdafx.h"
 #include "C_Hero.h"
 
-void C_Magician::Render(HDC memdc)
-{
-    //애니메이션 카운트 업데이트 하기
-    if (move_ == 1)
-        count = ++count % (N_IMAGES - 1);
-    else
-        count = 0;
-
-    //이미지DC 생성
-    HDC image_dc = CreateCompatibleDC(memdc);
-    //해당 애니메이션 프레임 이미지DC에 넣기
-    HBITMAP oldBit = (HBITMAP)SelectObject(image_dc, MagicianBit[count]);
-
-    //이미지DC의 사진을 임시DC의 적절한 곳에 넣기
-    //정확한 이미지 크기가 들어가야 제대로 나온다.
-    TransparentBlt(memdc, x_, y_, GRID_WH, GRID_WH, image_dc, 0, 0, MAGICIAN_W, MAGICIAN_H, WHITE);
-
-    //이미지 지우기
-    SelectObject(image_dc, oldBit);
-    //이미지DC 삭제
-    DeleteDC(image_dc);
-}
-
-void C_Magician::Move_Per_Frame(int dest_x, int dest_y)
+void C_Hero::Move_Per_Frame(int dest_x, int dest_y)
 {
     if (x_ == dest_x and y_ == dest_y) move_ = 2;
     if (move_ == 2) return;
@@ -49,4 +26,27 @@ void C_Magician::Move_Per_Frame(int dest_x, int dest_y)
         int move_y = 10 * dist_y / (int)sqrt(dist_x * dist_x + dist_y * dist_y);
         y_ += move_y;
     }
+}
+
+void C_Magician::Render(HDC memdc)
+{
+    //애니메이션 카운트 업데이트 하기
+    if (move_ == 1)
+        count = ++count % (N_IMAGES - 1);
+    else
+        count = 0;
+
+    //이미지DC 생성
+    HDC image_dc = CreateCompatibleDC(memdc);
+    //해당 애니메이션 프레임 이미지DC에 넣기
+    HBITMAP oldBit = (HBITMAP)SelectObject(image_dc, MagicianBit[count]);
+
+    //이미지DC의 사진을 임시DC의 적절한 곳에 넣기
+    //정확한 이미지 크기가 들어가야 제대로 나온다.
+    TransparentBlt(memdc, x_, y_, GRID_WH, GRID_WH, image_dc, 0, 0, MAGICIAN_W, MAGICIAN_H, WHITE);
+
+    //이미지 지우기
+    SelectObject(image_dc, oldBit);
+    //이미지DC 삭제
+    DeleteDC(image_dc);
 }
--- HeroChess/C_Hero.h
+++ HeroChess/C_Hero.h
@@ -5,20 +5,27 @@
 class C_Hero
 {
 public:
+    C_Hero(int x, int y) : x_(x), y_(y) {};
     virtual ~C_Hero() {}
     virtual void Render(HDC memdc) = 0;
-    virtual void Move_Per_Frame(int destX, int destY) = 0;
+    virtual void Move_Per_Frame(int destX, int destY);
     virtual int get_x() const = 0;
     virtual int get_y() const = 0;
     virtual void Set_Move(int move) = 0;
     virtual int Get_Move() const = 0;
+
+protected:
+    int x_;
+    int y_;
+    int move_ = 0;
+    int count = 0;
 };
 
 class C_Magician : public C_Hero
 {
 public:
     C_Magician(int x, int y) 
-        : x_(x), y_(y)
+		: C_Hero(x, y)
     {
         for (int i = 0; i < 17; i++)
             MagicianBit[i] = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_BITMAP2 + i));
@@ -29,7 +36,10 @@ public:
             DeleteObject(image);
     }
     virtual void Render(HDC memdc) override;
-    virtual void Move_Per_Frame(int dest_x, int dest_y) override;
+    virtual void Move_Per_Frame(int dest_x, int dest_y)
+    {
+        C_Hero::Move_Per_Frame(dest_x, dest_y);
+    }
 
     virtual int get_x() const override { return x_; }
     virtual int get_y() const override { return y_; }
@@ -37,53 +47,47 @@ public:
     virtual void Set_Move(int move) override { move_ = move; }
     virtual int Get_Move() const override { return move_; }
 private:
-    int x_;
-    int y_;
-
     static const int N_IMAGES = 17;
 
     HBITMAP MagicianBit[N_IMAGES];
 
-    int count = 0;
     const int MAGICIAN_W = 48;
     const int MAGICIAN_H = 48;
-    
-    int move_ = 0;
 };
 
-class C_Reaper : public C_Hero
-{
-public:
-    C_Reaper(int x, int y)
-        : x_(x), y_(y)
-    {
-        for (int i = 0; i < N_IMAGES; i++)
-            ReaperBit[i] = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_BITMAP19 + i));
-    }
-    virtual ~C_Reaper() override
-    {
-        for (auto& image : ReaperBit)
-            DeleteObject(image);
-    }
-    virtual void Render(HDC memdc) override;
-    virtual void Move_Per_Frame(int dest_x, int dest_y) override;
-
-    virtual int get_x() const override { return x_; }
-    virtual int get_y() const override { return y_; }
-
-    virtual void Set_Move(int move) override { move_ = move; }
-    virtual int Get_Move() const override { return move_; }
-private:
-    int x_;
-    int y_;
-
-    static const int N_IMAGES = 5;
-
-    HBITMAP ReaperBit[N_IMAGES];
-
-    int count = 0;
-    const int REAPER_W = 70;
-    const int REAPER_H = 75;
-
-    int move_ = 0;
-};
+//class C_Reaper : public C_Hero
+//{
+//public:
+//    C_Reaper(int x, int y)
+//        : x_(x), y_(y)
+//    {
+//        for (int i = 0; i < N_IMAGES; i++)
+//            ReaperBit[i] = LoadBitmap(hInst, MAKEINTRESOURCE(IDB_BITMAP19 + i));
+//    }
+//    virtual ~C_Reaper() override
+//    {
+//        for (auto& image : ReaperBit)
+//            DeleteObject(image);
+//    }
+//    virtual void Render(HDC memdc) override;
+//    virtual void Move_Per_Frame(int dest_x, int dest_y) override;
+//
+//    virtual int get_x() const override { return x_; }
+//    virtual int get_y() const override { return y_; }
+//
+//    virtual void Set_Move(int move) override { move_ = move; }
+//    virtual int Get_Move() const override { return move_; }
+//private:
+//    int x_;
+//    int y_;
+//
+//    static const int N_IMAGES = 5;
+//
+//    HBITMAP ReaperBit[N_IMAGES];
+//
+//    int count = 0;
+//    const int REAPER_W = 70;
+//    const int REAPER_H = 75;
+//
+//    int move_ = 0;
+//};
